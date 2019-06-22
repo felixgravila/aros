6e32554f614451cb212cf97d864dd5f761572a26
Felix Gravila <felixgravila@gmail.com>
2019-06-22 19:34:30 +0200

    :white_check_mark: Updating parsing and typecheck tests

--- arosc/test/Spec.hs
+++ arosc/test/Spec.hs
@@ -1,33 +1,52 @@
 import Test.Hspec
 import Syntax
 import Parser
+import Checker
+import Evaluator
 
+resDir :: String
+resDir = "test/resources/"
 
-resDir = "test/resources/" :: FilePath
+doTypeCheck :: [Char] -> IO Bool
+doTypeCheck file = do
+  code <- readFile (resDir ++ file)
+  let (Right ast) = parseAros file code
+  let (programOk,_) = runChecker ast
+  return programOk
 
 main :: IO ()
 main = hspec $ do
-  -- EXAMPLE
-  describe "Addition (Example)" $ do
-    let onePlusOne = 1 + 1
-
-    it "1 + 1 should be greater than 1" $ do
-      (onePlusOne > 1) `shouldBe` True
-    it "1 + 1 should equal 2" $ do
-      onePlusOne `shouldBe` 2
-
-
   describe "Parsing" $ do
     it "Basic shape declaration in grid def should parse correctly (simple-decl-and-grid)" $ do
       let file = "simple-decl-and-grid.aros"
       code <- readFile (resDir ++ file)
       let (Right ast) = (parseAros file code)
       let correctAst = Program
-                        [ SDecl
-                         "myShape" (UShape [ SMPoint (VVec (Vector (ILit 1) (ILit 1))) ])
-                        ]
-                        (GridDef
-                        (SShape
-                                (SVector (ILit 5) (ILit 5))
-                                [ SMIdent "myShape" (VVec (Vector (ILit 1) (ILit 2))) ]))
+                       [
+                         Decl TypeInt "myint" (BinaryExp (IntegerExp 5) Plus (IntegerExp 3)),
+                         Decl TypeVec "thevec" (VectorExp (VariableExp "myint") (IntegerExp 2))
+                       ]
+                       (GridDef (VectorExp (IntegerExp 4) (IntegerExp 4)) (VariableExp "place"))
+                       (RobotRoute (VectorExp (IntegerExp 1) (IntegerExp 1)) (VectorExp (IntegerExp 2) (IntegerExp 2)))
       ast `shouldBe` correctAst
+
+  describe "Type Checking" $ do
+    it "parsedVars should type check" $ do
+      didItTypecheck <- doTypeCheck "parsedVars.aros"
+      didItTypecheck `shouldBe` True
+    it "01-errors should not type check" $ do
+      didItTypecheck <- doTypeCheck "01-errors.aros"
+      didItTypecheck `shouldBe` False
+    it "02-recCurried should type check" $ do
+      didItTypecheck <- doTypeCheck "02-recCurried.aros"
+      didItTypecheck `shouldBe` True
+    it "03-CurryHoward should type check" $ do
+      didItTypecheck <- doTypeCheck "03-CurryHoward.aros"
+      didItTypecheck `shouldBe` True
+    it "04-properMap should type check" $ do
+      didItTypecheck <- doTypeCheck "04-properMap.aros"
+      didItTypecheck `shouldBe` True
+
+  describe "Evaluation" $ do
+    it "04-properMap should evaluate correctly" $ do
+      True `shouldBe` True
